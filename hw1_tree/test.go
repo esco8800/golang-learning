68a1c797cb62a4aa7d1f4544c73db1c212d66f4c
Илья Семёнов <ilya.semenov@chulakov.ru>
2020-11-29 21:35:28 +0300

    Правки

--- hw1_tree/test.go
+++ hw1_tree/test.go
@@ -8,32 +8,84 @@ import (
 	"os"
 )
 
+var res string
+var org int = 0
+
 func main() {
-	err := testDirTree(os.Stdout, ".", true)
+	err := testDirTree(os.Stdout, "testdata", true)
 	if err != nil {
 		panic(err.Error())
 	}
 }
 
 func testDirTree(out io.Writer, path string, printFiles bool) (error error)  {
-	files, err := ioutil.ReadDir(path)
-	res := ""
+
+	var level int = -1
+	var pos int = 1
+
+	err := testDirTreeRecursive(os.Stdout, path, printFiles, level, pos)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	for _, file := range files {
-		if file.IsDir() {
-			res = concat(res, "├───" + file.Name())
-			fmt.Println(res)
-			err := testDirTree(os.Stdout, ".", true)
-			if err != nil {
+	fmt.Println(res)
+	return nil
+}
+
+func testDirTreeRecursive(out io.Writer, path string, printFiles bool, level, pos int) (error error)  {
+	files, err := ioutil.ReadDir(path)
+	lenFiles := len(files)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	level++
+
+	for i := 0; i < lenFiles; i++ {
+
+		if lenFiles == i + 1 {
+			pos = 2
+		} else  {
+			pos = 1
+		}
+
+		if files[i].IsDir() {
+			makeByLevelAndPos(level, pos, files[i].Name())
+			err2 := testDirTreeRecursive(os.Stdout, concat(path + "/", files[i].Name()), printFiles, level, pos)
+			if err2 != nil {
 				log.Fatal(err)
 			}
+		} else {
+			if printFiles {
+				makeByLevelAndPos(level, pos, files[i].Name())
+			}
 		}
-		fmt.Println("├───", file.Name())
 	}
 	return nil
+}
+
+func makeByLevelAndPos(level, pos int, name string) {
+	for i := 0; i < level; i++ {
+
+		if org != 0  && org <= i {
+			res = concat(res, "\t")
+		} else {
+			res = concat(res, "|\t")
+		}
+
+	}
+
+	if level == org {
+		org = 0
+	}
+
+	if pos == 1 {
+		res = concat(res, "├───" + name + "\n")
+	}
+	if pos == 2 {
+		res = concat(res, "└───" + name + "\n")
+		org = level
+	}
 }
 
 func concat(str, concat string) string  {
